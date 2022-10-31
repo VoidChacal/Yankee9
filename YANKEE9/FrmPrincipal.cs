158ffeb7f36981f600213b585c7a3891c8cd1be4
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2022-10-31 16:18:44 -0300

    Add files via upload

---
+++ YANKEE9/FrmPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YANKEE9
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void usuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            YANKEE9Usuar fu = new YANKEE9Usuar();
+            fu.ShowDialog();
+        }
+    }
+}
